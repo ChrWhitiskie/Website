ed027aec48c81fbc1b323de92ee2a890e61f1346
Chris Whitiskie <chrwhitiski@student.pakuranga.school.nz>
2022-05-26 11:36:24 +1200

    Moives database

--- FlaskUserDemo/app.py
+++ FlaskUserDemo/app.py
@@ -11,42 +11,50 @@ def restrict():
     restricted_pages = ['dashboard', 'edit', 'delete', 'view_user']
     admin_only = ['list_users']
 
-    if 'logged_in' not in session and request.endpoint in restricted_pages:
-        flash("You are not logged in!")
-        return redirect('/login')
+    #if 'logged_in' not in session and request.endpoint in restricted_pages:
+    #    flash("You are not logged in!")
+    #    return redirect('/login')
     
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
+@app.route('/movies')
+def movies():
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM movies")
+            result = cursor.fetchall()
+    return render_template('movies.html', result=result)
 
-        password = request.form['password']
-        encrypted_passsword = hashlib.sha256(password.encode()).hexdigest()
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    if request.method == 'POST':
 
-        with create_connection() as connection:
-            with connection.cursor() as cursor:
-                sql = "SELECT * FROM users WHERE email=%s AND password=%s"
-                values = (
-                    request.form['email'],
-                    encrypted_passsword
-                )
-                cursor.execute(sql, values)
-                result = cursor.fetchone()
-        if result:
-            session['logged_in'] = True
-            session['first_name'] = result['first_name']
-            session['role'] = result['role']
-            session['id'] = result['id']
-            return redirect('/')
-        else:
-            flash("Invalid username or password!")
-            return redirect('/login')
-    else:
-        return render_template('login.html')
+#        password = request.form['password']
+#        encrypted_passsword = hashlib.sha256(password.encode()).hexdigest()
+
+#        with create_connection() as connection:
+#            with connection.cursor() as cursor:
+#                sql = "SELECT * FROM users WHERE email=%s AND password=%s"
+#                values = (
+#                    request.form['email'],
+#                    encrypted_passsword
+#                )
+#                cursor.execute(sql, values)
+#                result = cursor.fetchone()
+#        if result:
+#            session['logged_in'] = True
+#            session['first_name'] = result['first_name']
+#            session['role'] = result['role']
+#            session['id'] = result['id']
+#            return redirect('/')
+#        else:
+#            flash("Invalid username or password!")
+#            return redirect('/login')
+#    else:
+#        return render_template('login.html')
 
 @app.route('/logout')
 def logout():
@@ -72,21 +80,18 @@ def add_user():
         with create_connection() as connection:
             with connection.cursor() as cursor:
                 sql = """INSERT INTO users 
-                    (first_name, last_name, email, password, avatar)
-                    VALUES (%s, %s, %s, %s, %s)
+                    (first_name, last_name)
+                    VALUES (%s, %s)
                     """
                 values = (
                     request.form['first_name'], 
-                    request.form['last_name'], 
-                    request.form['email'], 
-                    encrypted_passsword,
-                    avatar_filename
+                    request.form['last_name']
                     )
                 try:
                     cursor.execute(sql, values)
                     connection.commit()
                 except pymysql.err.IntegrityError:
-                    flash('Email has already been taken')
+                    #flash('Email has already been taken')
                     return redirect('/register')
         return redirect('/')
     return render_template('users_add.html')
@@ -94,9 +99,9 @@ def add_user():
 # TODO: Add a '/dashboard' (list_users) route that uses SELECT
 @app.route('/dashboard')
 def dashboard():
-    if session['role'] != 'Admin':
-        flash("You are not an Admin!")
-        return redirect('/')
+#    if session['role'] != 'Admin':
+#        flash("You are not an Admin!")
+#        return redirect('/')
     with create_connection() as connection:
         with connection.cursor() as cursor:
             cursor.execute("SELECT * FROM users")
@@ -115,10 +120,10 @@ def view_user():
 # TODO: Add a '/delete_user' route that uses DELETE
 @app.route('/delete')
 def delete():
-    if session['role'] != 'Admin':
-        flash("You are not an Admin!")
-        return redirect('/')
-    else:
+    #if session['role'] != 'Admin':
+    #    flash("You are not an Admin!")
+    #    return redirect('/')
+    #else:
         with create_connection() as connection:
             with connection.cursor() as cursor:
                 sql = """DELETE FROM users WHERE id = %s"""
@@ -137,31 +142,27 @@ def edit():
     
     if request.method == 'POST':
         
-        if request.files['avatar'].filename:
-            avatar_image = request.files["avatar"]
-            ext = os.path.splitext(avatar_image.filename)[1]
-            avatar_filename = str(uuid.uuid4())[:8] + ext
-            avatar_image.save("static/images/" + avatar_filename)
-            if request.form['old_avatar'] != 'None' and os.path.exists("static/images/" + request.form['old_avatar']):
-                os.remove("static/images/" + request.form['old_avatar'])
-        elif request.form['old_avatar'] != 'None':
-            avatar_filename = request.form['old_avatar']
-        else:
-            avatar_filename = None
+       # if request.files['avatar'].filename:
+         #   avatar_image = request.files["avatar"]
+         #   ext = os.path.splitext(avatar_image.filename)[1]
+         #   avatar_filename = str(uuid.uuid4())[:8] + ext
+         #   avatar_image.save("static/images/" + avatar_filename)
+         #   if request.form['old_avatar'] != 'None' and os.path.exists("static/images/" + request.form['old_avatar']):
+         #       os.remove("static/images/" + request.form['old_avatar'])
+        #elif request.form['old_avatar'] != 'None':
+         #   avatar_filename = request.form['old_avatar']
+        #else:
+        #    avatar_filename = None
 
         with create_connection() as connection:
             with connection.cursor() as cursor:
                 sql = """UPDATE users SET
                     first_name = %s,
-                    last_name = %s,
-                    email = %s,
-                    avatar = %s
+                    last_name = %s
                 WHERE id = %s"""
                 values = (
                     request.form['first_name'],
                     request.form['last_name'],
-                    request.form['email'],
-                    avatar_filename,
                     request.form['id']
                 )
                 cursor.execute(sql, values)
